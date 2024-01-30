371352e6c4d9abd6bf7bacda7578cdd7751ea859
poffi <143832574+poffi@users.noreply.github.com>
2024-01-30 08:40:25 -0800

    saved

---
+++ michael-Anaebonam.js
@@ -0,0 +1,24 @@
+//Oranizing and storing data is a foundational concept of programming. example "NEW YEAR'S RESOLUTIONS"
+let newyearsresolutions = ['keep a journal', 'take a falconry class','learn Kubernetes'
+];
+newyearsresolutions[0]=' travelling'
+newyearsresolutions[1]=' Going to gym'
+newyearsresolutions[2]=' reading'
+//Arrays are javascript's way of maling lists
+//Arrays can store any data types
+//like lists, arrays are ordered, meaning each items has a numbered posistion.
+// to creat an Array is to use an Array literal
+//Array wraps items in a square brackets[]
+const hobbies = [ 'drawing' , 'Gaming' ,'reading'];
+console.log ( hobbies[0])
+// each element in your array has a numbered position known as its index
+//We can access individual items using their index.
+// Arrays in javaScript are Zero-indexed, meaning the position start counting from 0.
+// CITIES is an array that has three elements.
+let listItem = (hobbies[0])
+console.log( hobbies [2])
+
+let seasons =['winter','spring','summer','Fall'];
+seasons[3]= 'Autumn';
+console.log(seasons);
+console.log(newyearsresolutions)
